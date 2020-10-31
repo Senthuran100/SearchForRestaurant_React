a3e35553df11139991720dfcf60623cdf09ceefa
SENTHURAN <senthuran.manoharan@gmail.com>
2020-10-31 17:09:45 +0530

    Updates

---
+++ precache-manifest.3d22ade225e8fd61b6e267c031151836.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3917564b71421f5e6a05690f376059c7",
+    "url": "/SearchForRestaurant_React/index.html"
+  },
+  {
+    "revision": "21df1aab9e12826ddfd5",
+    "url": "/SearchForRestaurant_React/static/css/2.ceb6ed14.chunk.css"
+  },
+  {
+    "revision": "005d5f76dacbb9cfdf3c",
+    "url": "/SearchForRestaurant_React/static/css/main.7f3d0500.chunk.css"
+  },
+  {
+    "revision": "21df1aab9e12826ddfd5",
+    "url": "/SearchForRestaurant_React/static/js/2.d472a82c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/SearchForRestaurant_React/static/js/2.d472a82c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005d5f76dacbb9cfdf3c",
+    "url": "/SearchForRestaurant_React/static/js/main.a5043d13.chunk.js"
+  },
+  {
+    "revision": "f3f1e7b456f914f2675b",
+    "url": "/SearchForRestaurant_React/static/js/runtime-main.cba27886.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/SearchForRestaurant_React/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/SearchForRestaurant_React/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/SearchForRestaurant_React/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/SearchForRestaurant_React/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/SearchForRestaurant_React/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/SearchForRestaurant_React/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/SearchForRestaurant_React/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/SearchForRestaurant_React/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/SearchForRestaurant_React/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/SearchForRestaurant_React/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/SearchForRestaurant_React/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/SearchForRestaurant_React/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/SearchForRestaurant_React/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/SearchForRestaurant_React/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/SearchForRestaurant_React/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/SearchForRestaurant_React/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/SearchForRestaurant_React/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
